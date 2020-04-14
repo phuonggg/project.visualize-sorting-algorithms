59ffd33cf07d6a2290e7c9387bac5a28282041ad
phuonggg <tieuphuong.ftu@gmail.com>
2020-04-15 02:01:20 +0900

    finished basic requirements

--- src/Sort.js
+++ src/Sort.js
@@ -2,13 +2,34 @@ class SomeKindOfSort {
   constructor(array) {
     this.array = array;
   }
-  sort() {
-    console.log("sort!");
+  // sort() {
+  //   console.log("sort!");
+  // }
+
+  sort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let lowest = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[lowest]) {
+          lowest = j;
+        }
+      }
+      if (i !== lowest) {
+        //SWAP!
+        let temp = arr[i];
+        arr[i] = arr[lowest];
+        arr[lowest] = temp;
+      }
+    }
+    return arr;
   }
 
   returnValue(value) {
     return value;
   }
 }
+
+const sort = new SomeKindOfSort();
+console.log(sort.sort([0, 2, 34, 22, 10, 19, 17]));
 
 module.exports = SomeKindOfSort;
--- src/app.js
+++ src/app.js
@@ -10,17 +10,283 @@ const Sort = require("./Sort");
 require("./index.css");
 
 const sort = new Sort();
-sort.sort();
+// sort.sort();
 
-function createCheesyTitle(slogan) {
-  const container = document.createElement("h1");
-  const textNode = document.createTextNode(slogan);
-  container.appendChild(textNode);
-  return container;
+// function createCheesyTitle(slogan) {
+//   const container = document.createElement("h1");
+//   const textNode = document.createTextNode(slogan);
+//   container.appendChild(textNode);
+//   return container;
+// }
+
+// const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
+// document.getElementById("title").appendChild(title);
+
+const yo1 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "7";
+  textNode2.innerText = "5";
+  textNode3.innerText = "4";
+  textNode4.innerText = "2";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("min");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn1").addEventListener("click", yo1);
+
+const yo2 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "5";
+  textNode3.innerText = "4";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn2").addEventListener("click", yo2);
+
+const yo3 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "5";
+  textNode3.innerText = "4";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("min");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn3").addEventListener("click", yo3);
+
+const yo4 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "4";
+  textNode3.innerText = "5";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("sort");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn4").addEventListener("click", yo4);
+
+const yo5 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "4";
+  textNode3.innerText = "5";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("sort");
+  textNode3.classList.add("min");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn5").addEventListener("click", yo5);
+
+const yo6 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "4";
+  textNode3.innerText = "5";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("sort");
+  textNode3.classList.add("sort");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn6").addEventListener("click", yo6);
+
+const yo7 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "4";
+  textNode3.innerText = "5";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("sort");
+  textNode3.classList.add("sort");
+  textNode4.classList.add("min");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn7").addEventListener("click", yo7);
+
+const yo8 = () => {
+  const container1 = document.createElement("div");
+  const textNode1 = document.createElement("div");
+  const textNode2 = document.createElement("div");
+  const textNode3 = document.createElement("div");
+  const textNode4 = document.createElement("div");
+
+  textNode1.innerText = "2";
+  textNode2.innerText = "4";
+  textNode3.innerText = "5";
+  textNode4.innerText = "7";
+
+  container1.classList.add("container");
+  textNode1.classList.add("grid-item");
+  textNode2.classList.add("grid-item");
+  textNode3.classList.add("grid-item");
+  textNode4.classList.add("grid-item");
+
+  textNode1.classList.add("sort");
+  textNode2.classList.add("sort");
+  textNode3.classList.add("sort");
+  textNode4.classList.add("sort");
+
+  container1.appendChild(textNode1);
+  container1.appendChild(textNode2);
+  container1.appendChild(textNode3);
+  container1.appendChild(textNode4);
+
+  document.getElementById("title").appendChild(container1);
+};
+
+document.getElementById("myBtn8").addEventListener("click", yo8);
+
+{
+  /* <div class="container 1">
+<div class="grid-item orange min">7</div>
+<div class="grid-item green">5</div>
+<div class="grid-item purple">4</div>
+<div class="grid-item green">2</div>
+</div> */
 }
 
-const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
-document.getElementById("title").appendChild(title);
+// const step1 = [7, 5, 4, 2]
+// const step2 = [2, 5, 4, 7]
+// cosnt step3 = [2, 4, 5, 7]
+// const step4 = [2, 4, 5, 7]
 
 /*
     An simple example of how you can make your project a bit more
@@ -29,12 +295,12 @@ document.getElementById("title").appendChild(title);
     In our `index.html` page, we have a short form.
     Here is the code that talks to it.
   */
-function changeTitle(event) {
-  event.preventDefault();
-  // console.log('What is an event?', event);
-}
+// function changeTitle(event) {
+//   event.preventDefault();
+//   // console.log('What is an event?', event);
+// }
 
-const form = document.querySelector("form");
-document.addEventListener("DOMContentLoaded", () => {
-  form.onsubmit = changeTitle;
-});
+// const form = document.querySelector("form");
+// document.addEventListener("DOMContentLoaded", () => {
+//   form.onsubmit = changeTitle;
+// });
